f49ae08c0827109fc4239df39efb7dd76e20cfa6
asmsadat <u1804105@student.cuet.ac.bd>
2025-05-26 20:45:16 +0600

    Feat: add Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      
+      <Navbar />
       <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
         <Outlet />
       </main>
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header className="sticky top-0 z-50 bg-white shadow-md">
+      <div className="max-w-screen-2xl mx-auto px-4 py-4 md:py-6">
+        <nav className="flex flex-col md:flex-row justify-between items-center gap-4 md:gap-0">
+        {/* left */}
+        <div>
+          <Link to='/' className="text-2xl font-semibold text-blue-900 hover:text-blue-700 transition-colors">Student Portal</Link>
+        </div>
+
+        {/* center */}
+        <div className="flex items-center space-x-6 text-blue-700 text-base md:text-lg font-medium">
+          <Link to='/courses' className="hover:text-blue-500 transition-colors">Courses</Link>
+          <Link to='/students' className="hover:text-blue-500 transition-colors">Students</Link>
+          <Link to='/payments' className="hover:text-blue-500 transition-colors">Payments</Link>
+        </div>
+
+        {/* right */}
+        <div className='text-blue-600'>
+          <Link to='/signin' className="text-blue-700 hover:text-white hover:bg-blue-600 border border-blue-600 px-4 py-1 rounded-md transition-all duration-200">Sign in</Link>
+        </div>
+      </nav>
+      </div>
+      
+    </header>
+  )
+}
+
+export default Navbar
